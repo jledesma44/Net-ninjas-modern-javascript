cb7b417f3a746b473083144a918febe9271d742e
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-04-27 21:11:54 -0700

    Finished lesson-38

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,11 +1,11 @@
 // object literals
 
 let user = {
-  name: 'crystal',
+  name: "crystal",
   age: 30,
-  email: 'crystal@thenetninja.co.uk',
-  location: 'berlin',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite']
+  email: "crystal@thenetninja.co.uk",
+  location: "berlin",
+  blogs: ["why mac & cheese rules", "10 things to make with marmite"],
 };
 
 console.log(user);
@@ -14,8 +14,16 @@ console.log(user.age);
 user.age = 35;
 console.log(user.age);
 
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
+console.log(user["name"]);
+user["name"] = "chun-li";
+console.log(user["name"]);
 
 console.log(typeof user);
+
+let user = {
+  name: "Jaime",
+  age: 59,
+  email: "jedesma030@gmail.com",
+  location: "Los Angeles",
+  blog: ["niggas are for real", "absolutely the fuck"],
+};
