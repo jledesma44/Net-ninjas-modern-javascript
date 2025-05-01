b6b9ad00206a2fb20718464a5998fb1a626cd279
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-05-01 09:14:52 -0700

    Finished lesson-51

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,15 +1,17 @@
-// const content = document.querySelector('p');
-
+// const content = document.querySelector("p");
+//
 // console.log(content.classList);
-// content.classList.add('error');
-// content.classList.remove('success');
+// content.classList.add("error");
+// content.classList.remove("error");
+// content.classList.add("success");
 
-const paras = document.querySelectorAll('p');
+const paras = document.querySelectorAll("p");
 
-paras.forEach(p => {
-  if(p.textContent.includes('error')){
-    p.classList.add('error');
-  } else if(p.textContent.includes('success')) {
-    p.classList.add('success');
+paras.forEach((p) => {
+  if (p.textContent.includes("error")) {
+    p.classList.add("error");
+  }
+  if (p.textContent.includes("success")) {
+    p.classList.add("success");
   }
 });
